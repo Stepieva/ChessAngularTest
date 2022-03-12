4ff04fd277dbf07bf4a6f1601b35d923b94a1d2a
Steeee97 <spievaioli@yahoo.com>
2022-03-12 17:09:18 +0100

    Inserita scacchiera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgxChessBoardService} from 'ngx-chess-board';
+import {NgxChessBoardView} from 'ngx-chess-board';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  constructor(private ngxChessBoardService: NgxChessBoardService) { }
   title = 'ChessTest';
+  
+
+  @ViewChild('board', { static: false })
+  board!: NgxChessBoardView;
+
+
+   reset() {
+       this.board.reset();
+   }
+
 }
